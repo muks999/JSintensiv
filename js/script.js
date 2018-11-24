48392a0ddcf8a0ec14c1d547ba5b087353ae32c9
Muks <mukss@ya.ru>
2018-11-24 14:47:44 +0300

    Это наш первый комит.программа уже написана

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function myFirstApp(name, age) {
+
+		alert("Привет, меня зовут " + name + " и это моя первая программа!");
+
+		function showSkills() {
+			let skills = ['HTML', 'CSS', 'WP'];
+			for (let i = 0; i < skills.length; i++) {
+				document.write("Я владею " + skills[i] + "<br>");  //я владею HTML <br>
+			}
+		}
+		showSkills();
+
+		function checkAge() {
+				if (age > 18) {
+						document.write("Отлично, все дороги открыты!)" + "<br>");
+				} else{
+						document.write("Сорян, немножко нужно подрасти :(" + "<br>");
+				}
+		}
+
+		checkAge();
+
+		function calcPow(num) {
+				document.write(num * num);
+		}
+		calcPow(4);
+}
+
+myFirstApp("Max", 32);
+
+
+// let answers = [],
+// 		questions = [
+// 			"Как ваше имя?",
+// 			"Как ваше фамилия?",
+// 			"Сколько вам лет?"
+// 		];
+
+// for(let i = 0; i < questions.length; i++) {
+// 	answers[i] = prompt(questions[i])
+// }
+
+// document.write(answers)
+
+// // answers[0] = prompt("Как ваше имя?", "");
+// // answers[1] = prompt("Как ваше фамилия?", "");
+// // answers[2] = prompt("Сколько вам лет?", "");
+
+// // document.write(answers);
